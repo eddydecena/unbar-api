514b17d3e1be9c7234a34424226504702cd0a550
Eddy Perez <v-eddperez@expediagroup.com>
2023-01-29 01:01:53 -0400

    feat: add infra

---
+++ deployments/terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "unbar"
+
+    workspaces {
+      name = "gh-unbar-infra"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
---
+++ deployments/vars/us-east-1-eks-1-24.tfvars
@@ -0,0 +1,5 @@
+vpc_name = "unbar"
+region = "us-east-=1"
+
+cluster_name = "unbar"
+cluster_version = "1.24"
